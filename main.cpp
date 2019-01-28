9aa13c65d9841251f35b1bae4534856d92c9cd07
naokyan <47100121+naokyan@users.noreply.github.com>
2019-01-28 09:09:33 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+Test using GITHUB
+// Jiaqi Wang
+// Description: Hello World in C++
+//============================================================
+main
+
+#include <iostream>
+using namespace std;
+int hello1Function(string name1);
+int hello2Function(string name2);
+int main()
+{
+	string name1="Jiaqi Wang";
+	hello1Function(name1);
+	string name2="Just me";
+	hello2Function(name2);
+	return 0;
+}
