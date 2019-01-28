53a12c65f910faabdb6808357589038ad6e64383
naokyan <47100121+naokyan@users.noreply.github.com>
2019-01-28 09:12:37 -0800

    Create  hello1Function.cpp

---
+++ hello1Function.cpp
@@ -0,0 +1,15 @@
+hello1Function
+/*
+ * hello1Function.cpp
+ *
+ */
+#include <iostream>
+using namespace std;
+int hello1Function(string name1)
+{
+	cout << "hello World " << name1 << endl;
+	return 0;
+}
+
+
+hello1Function
